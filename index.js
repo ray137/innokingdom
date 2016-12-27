c47491f2f2c37293c175b54e2864c190dcf0f250
Ray Tong <ray137@gmail.com>
2016-12-27 14:16:00 +0800

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
+    res.send('This is innokingdom 1');
 });
 
 // Facebook Webhook
