8a9d41026ba069cf15d214b394ab56be029d2ad2
Ray Tong <ray137@gmail.com>
2016-12-27 14:04:01 +0800

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+const 
+express = require('express'),
+bodyParser = require('body-parser'),
+request = require('request');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
